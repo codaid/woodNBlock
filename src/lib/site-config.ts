4e6b40debfc5fa3e83956e096ec5470050af0e65
codaid <geffrey.dev@gmail.com>
2024-10-02 22:38:47 +0400

    back to last

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,5 +1,5 @@
 export const SiteConfig = {
-    title: "Wood&apos;N Block",
+    title: "Wood'N Block",
     description:
         "Le site qui vous permettra d'avoir enfin votre maison à un prix raisonnable !",
 } as const;
