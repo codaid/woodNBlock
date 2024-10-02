3521361b83d4918b93604a1fd692e83a9fb30577
codaid <geffrey.dev@gmail.com>
2024-10-02 22:32:07 +0400

    try corrige apos in title of site

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,5 +1,5 @@
 export const SiteConfig = {
-    title: "Wood'N Block",
+    title: "Wood&apos;N Block",
     description:
         "Le site qui vous permettra d'avoir enfin votre maison à un prix raisonnable !",
 } as const;
