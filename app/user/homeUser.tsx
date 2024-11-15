143b817354619f02942cc6606f4a256feaf7bfe4
codaid <geffrey.dev@gmail.com>
2024-11-15 21:04:27 +0400

    Update to push dev branch

--- app/user/homeUser.tsx
+++ app/user/homeUser.tsx
@@ -3,11 +3,9 @@ import HeroSection from "@/components/layout/home/hereoSection";
 const HomeUser = () => {
     return (
         <div className="relative flex min-h-screen flex-col">
-            {/* <Header /> */}
             <div className="flex-1 text-center">
                 <HeroSection />
             </div>
-            {/* <Footer /> */}
         </div>
     );
 };
